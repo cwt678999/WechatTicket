7a4964f755fd5c1cabec9e35c5583fe37511324d
Tai <taizhiwei16@163.com>
2018-10-18 20:55:27 +0800

    Update adminpage
    重写adminpage.views和urls

--- adminpage/views.py
+++ adminpage/views.py
@@ -14,19 +14,36 @@ from codex.baseerror import BaseError
 
 import json, datetime, time, os
 
-class Admin(APIView):
+class AdminLogin(APIView):
+    def get(self):
+        if self.request.user.is_authenticated():
+            return None
+        else:
+            e = BaseError(3, '')
+            raise e
 
+    def post(self):
+        usn = self.body['username']
+        pwd = self.body['password']
+        user = authenticate(username=usn, password=pwd)
+        if user is not None and user.is_active:
+            auth.login(self.request, user)
+            return None
+        e = BaseError(3, '')
+        raise e
+
+class AdminLogout(APIView):
+    def post(self):
+        auth.logout(self.request)
+        return None
+
+class AdminActivityList(APIView):
     def stringToTimeStamp(self, date_str):
         d = datetime.datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S")
         t = d.timetuple()
         timeStamp = int(time.mktime(t))
         timeStamp = float(str(timeStamp) + str("%06d" % d.microsecond)) / 1000000
         return int(timeStamp)
-
-    def timeStampToString(self, date_stamp):
-        d = datetime.datetime.fromtimestamp(float(date_stamp))
-        str = d.strftime("%Y-%m-%d %H:%M:%S.%f")
-        return str
 
     def formatActivityList(self, act_json_list):
         res_list = []
@@ -50,6 +67,54 @@ class Admin(APIView):
             res_list.append(tmp_dict)
         return res_list
 
+    def get(self):
+        activity_json = json.loads(serializers.serialize("json", Activity.objects.all()))
+        activity = self.formatActivityList(activity_json)
+        return activity
+
+class AdminActivityDelete(APIView):
+    def post(self):
+        del_id = self.body["id"]
+        del_act = Activity.objects.get(id=del_id)
+        if del_act is not None:
+            del_act.delete()
+            return None
+        else:
+            e = BaseError(2, '')
+            raise e
+
+class AdminActivityCreate(APIView):
+    def post(self):
+        act_info = self.body
+        new_act = Activity(name=act_info["name"], key=act_info["key"], place=act_info["place"],
+                           description=act_info["description"], start_time=act_info["startTime"],
+                           end_time=act_info["endTime"], book_start=act_info["bookStart"],
+                           book_end=act_info["bookEnd"], total_tickets=act_info["totalTickets"],
+                           status=act_info["status"], pic_url=act_info["picUrl"],
+                           remain_tickets=act_info["totalTickets"])
+
+        new_act.save()
+        id = new_act.id
+        return id
+
+class AdminImageUpload(APIView):
+    def post(self):
+        img = self.input['image'][0]
+        img_name = './media/img/%s' % (img.name)
+        print(os.getcwd())
+        with open(img_name, 'wb') as f:
+            for fimg in img.chunks():
+                f.write(fimg)
+        img_url = self.request.get_host() + self.request.path + img_name.strip('.')
+        return img_url
+
+class AdminActivityDetail(APIView):
+    def stringToTimeStamp(self, date_str):
+        d = datetime.datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S")
+        t = d.timetuple()
+        timeStamp = int(time.mktime(t))
+        timeStamp = float(str(timeStamp) + str("%06d" % d.microsecond)) / 1000000
+        return int(timeStamp)
 
     def formatActivityDetail(self, act_json):
         fields = act_json[0]['fields']
@@ -75,127 +140,54 @@ class Admin(APIView):
         return tmp_dict
 
     def get(self):
-        path = self.request.path[7:]
-
-        if path == "login":
-            if self.request.user.is_authenticated():
-                return None
-            else:
-                e = BaseError(3, '')
-                raise e
-
-        if path == "activity/list":
-            activity_json = json.loads(serializers.serialize("json", Activity.objects.all()))
-            activity = self.formatActivityList(activity_json)
-            return activity
-
-        if path == "activity/detail":
-            if self.request.user.is_authenticated == False:
-                e = BaseError(3, '')
-                raise e
-            act_id = self.query['id']
-            detail_queryset = Activity.objects.filter(id=act_id)
-            detail_json = json.loads(serializers.serialize("json", detail_queryset))
-            detail = self.formatActivityDetail(detail_json)
-            return detail
-
-        if path == "activity/menu":
-            res = []
-            act = Activity.objects.all()
-            current_stamp = int(time.time())
-            count = 1
-            for line in act:
-                start_stamp = time.mktime(line.book_start.timetuple())
-                end_stamp = time.mktime(line.book_end.timetuple())
-                if start_stamp < current_stamp and current_stamp < end_stamp:
-                    tmp_dict = {}
-                    tmp_dict['id'] = line.id
-                    tmp_dict['name'] = line.name
-                    tmp_dict['menuIndex'] = 0
-                    res.append(tmp_dict)
-            return res
+        act_id = self.query['id']
+        detail_queryset = Activity.objects.filter(id=act_id)
+        detail_json = json.loads(serializers.serialize("json", detail_queryset))
+        detail = self.formatActivityDetail(detail_json)
+        return detail
 
     def post(self):
-        path = self.request.path[7:]
+        new_act_info = self.body
+        act_id = new_act_info['id']
+        act = Activity.objects.get(id=act_id)
+        if act:
+            act.description = new_act_info['description']
+            act.pic_url = new_act_info['picUrl']
+            act.start_time = new_act_info['startTime']
+            act.end_time = new_act_info['endTime']
+            act.book_start = new_act_info['bookStart']
+            act.book_end = new_act_info['bookEnd']
+            act.total_tickets = new_act_info['totalTickets']
+            act.status = new_act_info['status']
+            act.save()
+        return None
 
-        if path == "login":
-            usn = self.body['username']
-            pwd = self.body['password']
-            user = authenticate(username=usn, password=pwd)
-            if user is not None and user.is_active:
-                auth.login(self.request, user)
-                return None
-            e = BaseError(3, '')
-            raise e
+class AdminActivityMenu(APIView):
+    def get(self):
+        res = []
+        act = Activity.objects.all()
+        current_stamp = int(time.time())
+        count = 1
+        for line in act:
+            start_stamp = time.mktime(line.book_start.timetuple())
+            end_stamp = time.mktime(line.book_end.timetuple())
+            if start_stamp < current_stamp and current_stamp < end_stamp:
+                tmp_dict = {}
+                tmp_dict['id'] = line.id
+                tmp_dict['name'] = line.name
+                tmp_dict['menuIndex'] = 0
+                res.append(tmp_dict)
+        return res
 
-        if path == "logout":
-            auth.logout(self.request)
-            return None
+    def post(self):
+        list = self.body
+        act_list = []
+        for update_id in list:
+            act = Activity.objects.get(id=update_id)
+            act_list.append(act)
+        CustomWeChatView.update_menu(act_list)
+        return None
 
-        if path == "activity/delete":
-            del_id = self.body["id"]
-            del_act = Activity.objects.get(id=del_id)
-            if del_act is not None:
-                del_act.delete()
-                return None
-            else:
-                e = BaseError(2, '')
-                raise e
-
-        if path == "activity/create":
-            act_info = self.body
-            new_act = Activity(name=act_info["name"], key=act_info["key"], place=act_info["place"],
-                               description=act_info["description"], start_time=act_info["startTime"],
-                               end_time=act_info["endTime"], book_start=act_info["bookStart"],
-                               book_end=act_info["bookEnd"], total_tickets=act_info["totalTickets"],
-                               status=act_info["status"], pic_url=act_info["picUrl"],
-                               remain_tickets=act_info["totalTickets"])
-
-            new_act.save()
-            id = new_act.id
-            return id
-
-        if path == "activity/delete":
-            del_id = self.body
-            del_act = Activity.objects.get(id=del_id)
-            if del_act is None:
-                e = BaseError(1, '')
-                raise e
-            del_act.delete()
-            return None
-
-        if path == "activity/detail":
-            new_act_info = self.body
-            act_id = new_act_info['id']
-            act = Activity.objects.get(id=act_id)
-            if act:
-                act.description = new_act_info['description']
-                act.pic_url = new_act_info['picUrl']
-                act.start_time = new_act_info['startTime']
-                act.end_time = new_act_info['endTime']
-                act.book_start = new_act_info['bookStart']
-                act.book_end = new_act_info['bookEnd']
-                act.total_tickets = new_act_info['totalTickets']
-                act.status = new_act_info['status']
-                act.save()
-            return None
-
-        if path == "activity/menu":
-            list = self.body
-            act_list = []
-            for update_id in list:
-                act = Activity.objects.get(id=update_id)
-                act_list.append(act)
-            CustomWeChatView.update_menu(act_list)
-            return None
-
-        if path == "image/upload":
-            img = self.input['image'][0]
-            img_name = './media/img/%s' % (img.name)
-            print(os.getcwd())
-            with open(img_name, 'wb') as f:
-                for fimg in img.chunks():
-                    f.write(fimg)
-            img_url = self.request.get_host() + self.request.path + img_name.strip('.')
-            return img_url
-
+class AdminActivityCheckin(APIView):
+    def post(self):
+        return None
