252c1eccab8b3a275cf04631a108a57c4a1a7dcc
ElvaWu <39610243+ElvaWu@users.noreply.github.com>
2018-10-19 21:23:39 +0800

    Update urls.py

--- adminpage/urls.py
+++ adminpage/urls.py
@@ -1,9 +1,22 @@
 # -*- coding: utf-8 -*-
 #
 
+from django.conf.urls import url
+from adminpage.views import AdminLogin, AdminLogout, AdminActivityList, AdminActivityDelete, \
+AdminActivityCreate, AdminActivityDetail, AdminActivityMenu, AdminImageUpload, AdminActivityCheckin
 
 
 __author__ = "Epsirom"
 
 
-urlpatterns = []
+urlpatterns = [
+    url(r'^login/?$', AdminLogin.as_view()),
+    url(r'^logout/?$', AdminLogout.as_view()),
+    url(r'^activity/list/?$', AdminActivityList.as_view()),
+    url(r'^activity/delete/?$', AdminActivityDelete.as_view()),
+    url(r'^activity/create/?$', AdminActivityCreate.as_view()),
+    url(r'^activity/detail/?$', AdminActivityDetail.as_view()),
+    url(r'^activity/menu/?$', AdminActivityMenu.as_view()),
+    url(r'^activity/checkin/?$', AdminActivityCheckin.as_view()),
+    url(r'^image/upload/?$', AdminImageUpload.as_view()),
+]
