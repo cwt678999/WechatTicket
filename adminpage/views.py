e9666ac4624df6d81cc42182ee82a0f812f36734
ElvaWu <39610243+ElvaWu@users.noreply.github.com>
2018-10-19 14:32:47 +0800

    Update views.py

--- adminpage/views.py
+++ adminpage/views.py
@@ -4,16 +4,197 @@ from django.shortcuts import render
 from codex.baseerror import *
 from codex.baseview import APIView
 
-from django.contrib.auth.models import User
+from django.contrib import auth
 from django.contrib.auth import authenticate
+from django.core import serializers
+
+from wechat.models import Activity
+from wechat.views import CustomWeChatView
+from codex.baseerror import BaseError
+
+import json, datetime, time, os
 
 class Admin(APIView):
 
+    def stringToTimeStamp(self, date_str):
+        d = datetime.datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S")
+        t = d.timetuple()
+        timeStamp = int(time.mktime(t))
+        timeStamp = float(str(timeStamp) + str("%06d" % d.microsecond)) / 1000000
+        return int(timeStamp)
+
+    def timeStampToString(self, date_stamp):
+        d = datetime.datetime.fromtimestamp(float(date_stamp))
+        str = d.strftime("%Y-%m-%d %H:%M:%S.%f")
+        return str
+
+    def formatActivityList(self, act_json_list):
+        res_list = []
+        for line in act_json_list:
+            tmp_dict = {}
+            fields = line['fields']
+            tmp_dict['id'] = line['pk']
+            tmp_dict['name'] = fields['name']
+            tmp_dict['description'] = fields['description']
+            tmp_time = fields['start_time'].replace("T", " ").replace("Z", "")
+            tmp_dict['startTime'] = self.stringToTimeStamp(tmp_time)
+            tmp_time = fields['end_time'].replace("T", " ").replace("Z", "")
+            tmp_dict['endTime'] = self.stringToTimeStamp(tmp_time)
+            tmp_dict['place'] = fields['place']
+            tmp_time = fields['book_start'].replace("T", " ").replace("Z", "")
+            tmp_dict['bookStart'] = self.stringToTimeStamp(tmp_time)
+            tmp_time = fields['book_end'].replace("T", " ").replace("Z", "")
+            tmp_dict['bookEnd'] = self.stringToTimeStamp(tmp_time)
+            tmp_dict['currentTime'] = int(time.time())
+            tmp_dict['status'] = fields['status']
+            res_list.append(tmp_dict)
+        return res_list
+
+
+    def formatActivityDetail(self, act_json):
+        fields = act_json[0]['fields']
+        tmp_dict = {}
+        tmp_dict['name'] = fields['name']
+        tmp_dict['key'] = fields['key']
+        tmp_dict['description'] = fields['description']
+        tmp_time = fields['start_time'].replace("T", " ").replace("Z", "")
+        tmp_dict['startTime'] = self.stringToTimeStamp(tmp_time)
+        tmp_time = fields['end_time'].replace("T", " ").replace("Z", "")
+        tmp_dict['endTime'] = self.stringToTimeStamp(tmp_time)
+        tmp_dict['place'] = fields['place']
+        tmp_time = fields['book_start'].replace("T", " ").replace("Z", "")
+        tmp_dict['bookStart'] = self.stringToTimeStamp(tmp_time)
+        tmp_time = fields['book_end'].replace("T", " ").replace("Z", "")
+        tmp_dict['bookEnd'] = self.stringToTimeStamp(tmp_time)
+        tmp_dict['totalTickets'] = fields['total_tickets']
+        tmp_dict['picUrl'] = fields['pic_url']
+        tmp_dict['bookedTickets'] = fields['total_tickets'] - fields['remain_tickets']
+        tmp_dict['usedTickets'] = 0
+        tmp_dict['currentTime'] = int(time.time())
+        tmp_dict['status'] = fields['status']
+        return tmp_dict
+
     def get(self):
-        return
+        path = self.request.path[7:]
+
+        if path == "login":
+            if self.request.user.is_authenticated():
+                return None
+            else:
+                e = BaseError(3, '')
+                raise e
+
+        if path == "activity/list":
+            activity_json = json.loads(serializers.serialize("json", Activity.objects.all()))
+            activity = self.formatActivityList(activity_json)
+            return activity
+
+        if path == "activity/detail":
+            if self.request.user.is_authenticated == False:
+                e = BaseError(3, '')
+                raise e
+            act_id = self.query['id']
+            detail_queryset = Activity.objects.filter(id=act_id)
+            detail_json = json.loads(serializers.serialize("json", detail_queryset))
+            detail = self.formatActivityDetail(detail_json)
+            return detail
+
+        if path == "activity/menu":
+            res = []
+            act = Activity.objects.all()
+            current_stamp = int(time.time())
+            count = 1
+            for line in act:
+                start_stamp = time.mktime(line.book_start.timetuple())
+                end_stamp = time.mktime(line.book_end.timetuple())
+                if start_stamp < current_stamp and current_stamp < end_stamp:
+                    tmp_dict = {}
+                    tmp_dict['id'] = line.id
+                    tmp_dict['name'] = line.name
+                    tmp_dict['menuIndex'] = 0
+                    res.append(tmp_dict)
+            return res
 
     def post(self):
-        usn = self.body.username
-        pwd = self.body.password
-        user = authenticate(username=usn, password=pwd)
-return
+        path = self.request.path[7:]
+
+        if path == "login":
+            usn = self.body['username']
+            pwd = self.body['password']
+            user = authenticate(username=usn, password=pwd)
+            if user is not None and user.is_active:
+                auth.login(self.request, user)
+                return None
+            e = BaseError(3, '')
+            raise e
+
+        if path == "logout":
+            auth.logout(self.request)
+            return None
+
+        if path == "activity/delete":
+            del_id = self.body["id"]
+            del_act = Activity.objects.get(id=del_id)
+            if del_act is not None:
+                del_act.delete()
+                return None
+            else:
+                e = BaseError(2, '')
+                raise e
+
+        if path == "activity/create":
+            act_info = self.body
+            new_act = Activity(name=act_info["name"], key=act_info["key"], place=act_info["place"],
+                               description=act_info["description"], start_time=act_info["startTime"],
+                               end_time=act_info["endTime"], book_start=act_info["bookStart"],
+                               book_end=act_info["bookEnd"], total_tickets=act_info["totalTickets"],
+                               status=act_info["status"], pic_url=act_info["picUrl"],
+                               remain_tickets=act_info["totalTickets"])
+
+            new_act.save()
+            id = new_act.id
+            return id
+
+        if path == "activity/delete":
+            del_id = self.body
+            del_act = Activity.objects.get(id=del_id)
+            if del_act is None:
+                e = BaseError(1, '')
+                raise e
+            del_act.delete()
+            return None
+
+        if path == "activity/detail":
+            new_act_info = self.body
+            act_id = new_act_info['id']
+            act = Activity.objects.get(id=act_id)
+            if act:
+                act.description = new_act_info['description']
+                act.pic_url = new_act_info['picUrl']
+                act.start_time = new_act_info['startTime']
+                act.end_time = new_act_info['endTime']
+                act.book_start = new_act_info['bookStart']
+                act.book_end = new_act_info['bookEnd']
+                act.total_tickets = new_act_info['totalTickets']
+                act.status = new_act_info['status']
+                act.save()
+            return None
+
+        if path == "activity/menu":
+            list = self.body
+            act_list = []
+            for update_id in list:
+                act = Activity.objects.get(id=update_id)
+                act_list.append(act)
+            CustomWeChatView.update_menu(act_list)
+            return None
+
+        if path == "image/upload":
+            img = self.input['image'][0]
+            img_name = './media/img/%s' % (img.name)
+            print(os.getcwd())
+            with open(img_name, 'wb') as f:
+                for fimg in img.chunks():
+                    f.write(fimg)
+            img_url = self.request.get_host() + self.request.path + img_name.strip('.')
+return img_url
