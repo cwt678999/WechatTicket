021484f7f4b2de122c1cea9c88179e31c4f1fe9d
ElvaWu <39610243+ElvaWu@users.noreply.github.com>
2018-10-19 14:18:18 +0800

    Update views.py

--- adminpage/views.py
+++ adminpage/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from codex.baseerror import *
+from codex.baseview import APIView
+
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+
+class Admin(APIView):
+
+    def get(self):
+        return
+
+    def post(self):
+        usn = self.body.username
+        pwd = self.body.password
+        user = authenticate(username=usn, password=pwd)
+return
