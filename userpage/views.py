197e2ac37b434043e5b7fc1216d6f33c484619ee
Tai <taizhiwei16@163.com>
2018-10-19 18:07:11 +0800

    Merge branch 'master' into Tai

--- userpage/views.py
+++ userpage/views.py
@@ -2,16 +2,26 @@ from codex.baseerror import *
 from codex.baseview import APIView
 
 from wechat.models import User
+import time,datetime
 
+
+def timeStamp(date_str):
+    d = datetime.datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S")
+    t = d.timetuple()
+    timestamp = int(time.mktime(t))
+    timestamp = float(str(timestamp) + str("%06d" % d.microsecond)) / 1000000
+    return int(timestamp)
 
 class UserBind(APIView):
 
     def validate_user(self):
-        """
-        input: self.input['student_id'] and self.input['password']
-        raise: ValidateError when validating failed
-        """
-        raise NotImplementedError('You should implement UserBind.validate_user method')
+        user = User.get_by_openid(self.input['openid'])
+        sduId = user.student_id
+        pswd = user.password
+        if stuId == self.input['student_id'] and pswd == self.input['password'] :
+            return
+        else :
+            raise NotImplementedError('You should implement UserBind.validate_user method')
 
     def get(self):
         self.check_input('openid')
@@ -23,3 +33,51 @@ class UserBind(APIView):
         self.validate_user()
         user.student_id = self.input['student_id']
         user.save()
+        
+class ActivityDetail(APIView):
+
+    def get(self):
+        self.check_input('id')
+        activity = Activity.objects.get(id=self.input['id'])
+        if activity.status == Activity.STATUS_PUBLISHED:
+            ActivityDict={}
+            ActivityDict['name']=activity.name
+            ActivityDict['key']=activity.key
+            ActivityDict['description']=activity.description
+            ActivityDict['startTime']=timeStamp(activity.start_time)
+            ActivityDict['endTime']=timeStamp(activity.end_time)
+            ActivityDict['place']=activity.place
+            ActivityDict['bookStart']=timeStamp(activity.book_start)
+            ActivityDict['bookEnd']=timeStamp(activity.book_end)
+            ActivityDict['totalTickets']=activity.total_tickets
+            ActivityDict['picUrl']=activity.pic_url
+            ActivityDict['remainTickets']=activity.remain_tickets
+            ActivityDict['currentTime']=int(time.time())
+            return ActivityDict
+        else:
+            raise LogicError(self.input)
+
+
+class TicketDetail(APIView):
+
+    def get(self):
+        self.check_input('openid', 'ticket')
+        ticket = Ticket.objects.get(unique_id=self.input['ticket'])
+        TicketDict={}
+        TicketDict['activityName']=ticket.activity.name
+        TicketDict['place']=ticket.activity.place
+        TicketDict['activityKey'] = ticket.activity.key
+        TicketDict['uniqueId'] = ticket.unique_id
+        TicketDict['startTime'] = timeStamp(ticket.activity.start_time)
+        TicketDict['endTime'] = timeStamp(ticket.activity.end_time)
+        TicketDict['currentTime'] = int(time.time())
+        TicketDict['status'] = ticket.status
+        return TicketDict
+
+
+
+
+
+
+
+
